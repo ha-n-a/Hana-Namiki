baabc4b98da00d1c245a4bf44ada8cd3a0ce8ec8
hana-yomogi <61237604+hana-yomogi@users.noreply.github.com>
2020-02-24 20:58:13 -0500

    Add files via upload

---
+++ regression.py
@@ -0,0 +1,60 @@
+'''
+Hana Namiki
+This is a regression model of relationship between houses' price and areas.
+x represents its area(square feet)and y represents the house price in $1000s.
+This data set is from https://slideplayer.com/slide/5291816/
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def estimate_coef(x, y):
+   #number of observations
+    n = np.size(x);
+
+    #finding the mean of x and y vector
+    mean_x, mean_y = np.mean(x), np.mean(y)
+
+    #calculating the least squares
+    SS_xy = np.sum(y*x) - n * mean_y * mean_x
+    SS_xx = np.sum(x*x) - n * mean_x * mean_x
+
+    #regression coefficents
+    slope = SS_xy/SS_xx
+    yintercept = mean_y - slope * mean_x
+
+    #return m and b
+    return(slope, yintercept)
+
+def plot_regression_line(x, y, b):
+    #plotting the actual points as a scatter plot
+    plt.scatter(x, y, color = "m", marker = "o", s = 30)
+
+    #predicted response vector
+    y_pred = b[0] + b[1] + x
+
+    #plotting the regression plot_regression_line
+    plt.plot(x, y_pred, color = "g")
+
+    #putting labels
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    #function to show plotting
+    plt.show()
+
+def main():
+    #observations
+    x = np.array([1400, 1600, 1700, 1875, 1100, 1550, 2350, 2450, 1425, 1700])
+    y = np.array([245, 312, 279, 308, 199, 219, 405, 324, 319, 255])
+
+    # estimated coefficients
+    b = estimate_coef(x, y)
+    print("Estimated coefficients: \n b = {} \ \n m = {}".format(b[0], b[1]))
+
+    #plotting regression line
+    plot_regression_line(x, y, b)
+
+#make script importable and executables
+if __name__ == "__main__":
+    main()
